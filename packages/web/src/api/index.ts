35c5e1cd6f7af79f36d0dfd86decc7764d2bd780
Brazil Kayl <161436524+imissher6@users.noreply.github.com>
2026-05-13 07:09:59 +0800

    kapoya oie

--- packages/web/src/api/index.ts
+++ packages/web/src/api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { db } from "./database";
-import * as schema from "./database/schema";
+import { db } from "./database/index.js";
+import * as schema from "./database/schema.js";
 import { eq } from "drizzle-orm";
 import { nanoid } from "nanoid";
 
